51f4a1d8eaa67f4da9b0653aecfbdc1561de5f6e
Loredo Sola <loredo@brightmind.com>
2020-12-09 19:49:51 -0500

    Rafsan 1 - from VS!

--- MindfulMinuteDemo/ViewController.swift
+++ MindfulMinuteDemo/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
         let endTime = startTime.addingTimeInterval(1.0 * 60.0)
 
         self.saveMindfullAnalysis(startTime: startTime, endTime: endTime)
+        print("xxx startTime: \(startTime)")
     }
 
     override func viewDidLoad() {
@@ -31,86 +32,34 @@ class ViewController: UIViewController {
     }
 
     func activateHealthKit() {
-        // Define what HealthKit data we want to ask to read
-        let typestoRead = Set([
-            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession)!
-            ])
-
         // Define what HealthKit data we want to ask to write
         let typestoShare = Set([
             HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession)!
             ])
 
-        // Prompt the User for HealthKit Authorization
-        self.healthStore.requestAuthorization(toShare: typestoShare, read: typestoRead) { (success, error) -> Void in
+        // If necessary, Prompt the User for HealthKit Write Authorization
+        self.healthStore.requestAuthorization(toShare: typestoShare, read: nil) { (success, error) -> Void in
             if !success{
-                print("HealthKit Auth error\(error)")
+                print("HealthKit Auth error\(String(describing: error))")
             }
-            self.retrieveMindFulMinutes()
+            // self.retrieveMindFulMinutes()
         }
     }
 
-    func calculateTotalTime(sample: HKSample) -> TimeInterval {
-        let totalTime = sample.endDate.timeIntervalSince(sample.startDate)
-        let wasUserEntered = sample.metadata?[HKMetadataKeyWasUserEntered] as? Bool ?? false
-
-        print("\nHealthkit mindful entry: \(sample.startDate) \(sample.endDate) - value: \(totalTime) quantity: \(totalTime) user entered: \(wasUserEntered)\n")
-
-        return totalTime
-    }
-
-    func updateMeditationTime(query: HKSampleQuery, results: [HKSample]?, error: Error?) {
-        if error != nil {return}
-
-        // Sum the meditation time
-        let totalMeditationTime = results?.map(calculateTotalTime).reduce(0, { $0 + $1 }) ?? 0
-
-        print("\n Total: \(totalMeditationTime)")
-
-        renderMeditationMinuteText(totalMeditationSeconds: totalMeditationTime)
-
-    }
-
-    func renderMeditationMinuteText(totalMeditationSeconds: Double) {
-        let minutes = Int(totalMeditationSeconds / 60)
-        let labelText = "\(minutes) Mindful Minutes in the last 24 hours"
-        DispatchQueue.main.async {
-            self.meditationMinutesLabel.text = labelText
-        }
-    }
-
-    func retrieveMindFulMinutes() {
-
-        // Use a sortDescriptor to get the recent data first (optional)
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
-
-        // Get all samples from the last 24 hours
-        let endDate = Date()
-        let startDate = endDate.addingTimeInterval(-1.0 * 60.0 * 60.0 * 24.0)
-        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
-
-        // Create the HealthKit Query
-        let query = HKSampleQuery(
-            sampleType: mindfulType!,
-            predicate: predicate,
-            limit: 0,
-            sortDescriptors: [sortDescriptor],
-            resultsHandler: updateMeditationTime
-        )
-        // Execute our query
-        healthStore.execute(query)
-    }
-
+    //
+    // Save a meditation to HealthKit,  mindful minutes will be calculated from difference between start and end times.
+    // In HKCategorySample, value will always be 0 since the Meditation Minutes are calculated by HelthKit
+    //
     func saveMindfullAnalysis(startTime: Date, endTime: Date) {
         // Create a mindful session with the given start and end time
-        let mindfullSample = HKCategorySample(type:mindfulType!, value: 0, start: startTime, end: endTime)
+        let mindfullSample = HKCategorySample(type:mindfulType!,  value: 0, start: startTime, end: endTime)
 
         // Save it to the health store
         healthStore.save(mindfullSample, withCompletion: { (success, error) -> Void in
             if error != nil {return}
 
-            print("New data was saved in HealthKit: \(success)")
-            self.retrieveMindFulMinutes()
+            print("New data was saved in HealthKit: \(success) start: \(startTime) end: \(endTime)")
+            // self.retrieveMindFulMinutes()
         })
     }
 
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# iOS HealthKit Demo
-A tutorial meant to show how you can easily read and write from Apple's HealthKit
+# iOS HealthKit Demo:
+How to write to Apple's HealthKit
 
-## Tutorial
-[How to read and write Mindful Minutes from iOS’s HealthKit with Swift](https://medium.freecodecamp.org/read-write-mindful-minutes-from-healthkit-with-swift-232b65118fe2)
+## Notes:
+
+1. Run MindfulMinuteDemo in XCode & Emulator/iPhone 8.
+2. Click "Add a Mindful Minute to HealthKit"
+3. Check HealthKit to verify Mindful Minutes are getting recorded by starting Health app
+4. Health -> Summary screen -> Show all Health Data button -> Scroll down the data list to "Mindful Minutes".
+5. Each time you click the Add link it will add another minute to Health data.  But you must wait at least 3 minutes between clicks.
+
+Most of the code in ViewController.swift will be very useful for the integration.  HealthKit needs to be initialized. The app needs to get permission to write to Healthkit. Then app can write the session start and end times.  HealthKit will calculate the number of minutes between these two date/time values.
+
+
